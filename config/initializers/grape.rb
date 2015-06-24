76c1f907abe226562e372f6466d139456a15ed1e
Brian Dorton <dorton@gmail.com>
2015-06-24 10:18:22 -0500

    changes grape url

--- config/initializers/grape.rb
+++ config/initializers/grape.rb
@@ -1,3 +1,3 @@
 GrapeSwaggerRails.options.url = "/api/v1/swagger_doc"
 GrapeSwaggerRails.options.app_name = "Demo Day Students"
-GrapeSwaggerRails.options.app_url = "http://tiydemoday.herokuapp.com"
+GrapeSwaggerRails.options.app_url = "#{ENV['APPLICATION_URL']}"
