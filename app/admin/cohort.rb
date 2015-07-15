e902d0405cce6700553411cd40d9d0159d323917
Brian Dorton <dorton@gmail.com>
2015-07-15 15:48:57 -0500

    adds address permit_params to active admin

--- app/admin/cohort.rb
+++ app/admin/cohort.rb
@@ -3,7 +3,8 @@ ActiveAdmin.register Cohort do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-  permit_params :demo_day_date, :city, :venue_name, :address
+  permit_params :demo_day_date, :city, :venue_name, :venue_street,
+              :venue_city, :venue_zip
 
 
 end
