1453677d641916f592d60b5591fd3b312e653d12
Brian Dorton <dorton@gmail.com>
2015-07-21 11:13:06 -0500

    adds students to cohort at the projects level

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,7 +1,7 @@
 class Cohort < ActiveRecord::Base
 
   has_many :projects, dependent: :destroy
-
+  has_many :students, through: :projects
   validates :city, :demo_day_date, presence: true
 
 
--- app/serializers/cohort_serializer.rb
+++ app/serializers/cohort_serializer.rb
@@ -2,17 +2,19 @@ class CohortSerializer < ActiveModel::Serializer
 
 
   attributes :id, :demo_day_date, :city, :display_name, :venue_name, :venue_street,
-              :venue_city, :venue_zip, :projects
+              :venue_city, :venue_zip, :projects, :students
 
 has_many :projects
-
-  # def projects
-  #   object.projects.order(:app_name).pluck(:id)
-  # end
+has_many :students
 
   def demo_day_date
     object.demo_day_date.strftime("%b %d, %Y")
   end
+
+
+  # def students
+  #   object.students
+  # end
 
 
 
