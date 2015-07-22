f951e70e963bb6a68472d36ca499048ea5c4708e
Brian Dorton <dorton@gmail.com>
2015-07-22 14:21:44 -0500

    adds bio to student again

--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -2,7 +2,7 @@ class StudentSerializer < ActiveModel::Serializer
 
   attributes :id, :name, :course, :demo_day_date, :city, :portfolio_url, :email, :github_url, :app_name, :app_url, :rationale,
   :tech_used, :profile_pic_link, :group_project, :venue_name, :venue_street,
-  :created_at, :updated_at, :venue_city, :venue_zip
+  :created_at, :updated_at, :venue_city, :venue_zip, :bio
 
   def venue_name
     object.project.cohort.venue_name
