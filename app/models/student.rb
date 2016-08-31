dad0b6ab68c1e1117458a183de474b19c6cd1606
Brian Dorton <dorton@gmail.com>
2016-08-31 12:11:11 -0500

    removes portfolio validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ActiveRecord::Base
   belongs_to :project, touch: true
   has_one :cohort, through: :project
-  validates_presence_of :name, :course, :portfolio_url, :email, :github_url, :profile_pic_link, :project, :bio
+  validates_presence_of :name, :course, :email, :github_url, :profile_pic_link, :project, :bio
 
   scope :city, lambda { |city|  joins(:project => :cohort).where("cohorts.city = ?", city)   }
 
